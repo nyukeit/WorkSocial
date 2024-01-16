141bcebc74b6d48d5e9fd8c0be718719aacaf44a
bilel zaaraoui <72788978+Stinger1369@users.noreply.github.com>
2024-01-16 10:50:48 +0100

    Ajout du barNav et ces link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BarNav from "./components/BarNav/BarNav";
+
+import HomeScreen from "./pages/HomeScreen/HomeScreen";
+import PostScreen from "./pages/PostScreen/PostScreen";
+import ConnexionScreen from "./pages/ConnexionScreen/ConnexionScreen";
+import InscriptionScreen from "./pages/InscriptionScreen/InscriptionScreen";
+import EventsScreen from "./pages/EventsScreen/EventsScreen";
+import Sendage from "./pages/SendageScreen/SendageScreen";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <Router>
+      <BarNav />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/HomeScreen" element={<HomeScreen />} />
+        <Route path="/PostScreen" element={<PostScreen />} />
+        <Route path="/ConnexionScreen" element={<ConnexionScreen />} />
+        <Route path="/InscriptionScreen" element={<InscriptionScreen />} />
+        <Route path="/EventsScreen" element={<EventsScreen />} />
+        <Route path="/Sendage" element={<Sendage />} />
+      </Routes>
+    </Router>
   );
 }
 
