b333e052a5c19ebdfbf27e0e50280ae041404822
Nyukeit <nyukeit@outlook.com>
2024-01-22 21:53:57 +0100

    [FEAT] Contexts - Post
    Changes:
    1. Utiliser Context pour les Posts.
    2. Ajouter le PostProvider dans les routes
    3. Change les 'let' a 'const' dans le Hostname.js
    4. Simplifier le PostScreen pour just creer un post et afficher

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,26 +11,64 @@ import SendageScreen from "./pages/SendageScreen/SendageScreen";
 import MembersScreen from "./pages/MembersScreen/MembersScreen";
 import MyProfileScreen from "./pages/MyProfileScreen/MyProfileScreen";
 import { AuthProvider } from "./utils/useConnecte";
+import { PostProvider } from "./contexts/PostContext";
 
 import "./App.css";
 
 function App() {
   return (
+    // <React.StrictMode>
+    //   <AuthProvider>
+    //     <Router>
+    //       <BarNav />
+    //       <Routes>
+    //         <Route path="/" element={<HomeScreen />} />
+    //         <Route path="/HomeScreen" element={<HomeScreen />} />
+    //         <PostProvider>
+    //           <Route path="/posts" element={<PostScreen />} />
+    //         </PostProvider>
+    //         <Route path="/ConnexionScreen" element={<ConnexionScreen />} />
+    //         <Route path="/InscriptionScreen" element={<InscriptionScreen />} />
+    //         <Route path="/EventsScreen" element={<EventsScreen />} />
+    //         <Route path="/SendageScreen" element={<SendageScreen />} />
+    //         <Route path="/MembersScreen" element={<MembersScreen />} />
+    //         <Route path="/profile/:userId" element={<MyProfileScreen />} />
+    //       </Routes>
+    //     </Router>
+    //   </AuthProvider>
+    // </React.StrictMode>
     <React.StrictMode>
       <AuthProvider>
         <Router>
-          <BarNav />
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/HomeScreen" element={<HomeScreen />} />
-            <Route path="/posts" element={<PostScreen />} />
-            <Route path="/ConnexionScreen" element={<ConnexionScreen />} />
-            <Route path="/InscriptionScreen" element={<InscriptionScreen />} />
-            <Route path="/EventsScreen" element={<EventsScreen />} />
-            <Route path="/SendageScreen" element={<SendageScreen />} />
-            <Route path="/MembersScreen" element={<MembersScreen />} />
-            <Route path="/profile/:userId" element={<MyProfileScreen />} />
-          </Routes>
+          <>
+            <BarNav />
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/HomeScreen" element={<HomeScreen />} />
+
+              {/* Wrap BarNav and Routes with React.Fragment */}
+              <Route
+                path="/posts"
+                element={
+                  // <React.Fragment>
+                  <PostProvider>
+                    <PostScreen />
+                  </PostProvider>
+                  // </React.Fragment>
+                }
+              />
+
+              <Route path="/ConnexionScreen" element={<ConnexionScreen />} />
+              <Route
+                path="/InscriptionScreen"
+                element={<InscriptionScreen />}
+              />
+              <Route path="/EventsScreen" element={<EventsScreen />} />
+              <Route path="/SendageScreen" element={<SendageScreen />} />
+              <Route path="/MembersScreen" element={<MembersScreen />} />
+              <Route path="/profile/:userId" element={<MyProfileScreen />} />
+            </Routes>
+          </>
         </Router>
       </AuthProvider>
     </React.StrictMode>
