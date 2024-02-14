d41fc8bb374b2fbd60c8e2b40648b63212c480c2
Nyukeit <nyukeit@outlook.com>
2024-02-14 09:48:16 +0100

    [FIX] User Redirect to Dashboard #68

--- frontend/src/pages/LandingScreen/LandingScreen.jsx
+++ frontend/src/pages/LandingScreen/LandingScreen.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-function LandingScreen() {
+export default function LandingScreen() {
+  const navigate = useNavigate();
+  const user = localStorage.getItem("userId");
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  });
   return (
     <div className="container">
       <h1>Welcome to WorkSocial</h1>
     </div>
   );
 }
-
-export default LandingScreen;
