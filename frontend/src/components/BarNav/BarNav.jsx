8922e27770c1c4568f68df27cb7a7fe92b2d39d8
Nyukeit <nyukeit@outlook.com>
2024-02-14 11:14:46 +0100

    [FIX] Console Logs + Date Size
    Dans ce commit:
    🗸 Removed console logs from Connexion Screen
    🗸 Reduced text size of Date from BarNav
    🗸 Removed commented text from Hostname.js

--- frontend/src/components/BarNav/BarNav.jsx
+++ frontend/src/components/BarNav/BarNav.jsx
@@ -8,18 +8,6 @@ import { hostname } from "../../HostnameConnect/Hostname";
 import DropdownMenu from "./DropdownMenu";
 
 export default function BarNav() {
-  // const fetchWeather = async () => {
-  //   const response = await fetch(
-  //     "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
-  //   );
-  //   const data = await response.json();
-  //   console.info(data);
-  // };
-
-  // useEffect(() => {
-  //   fetchWeather();
-  // });
-
   const { isLoggedIn, logout } = useAuth();
   const user = isLoggedIn ? JSON.parse(localStorage.getItem("user")) : null;
   const navigate = useNavigate();
